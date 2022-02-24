639c697b1cf78ffeb43755df4cdde75bf786ba8f
Tanner <troxs690@gmail.com>
2022-02-24 15:53:04 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+   images () 
+    dogs () 
+  
+
+
+  function images ()  {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(imgUrl)
+.then(resp => resp.json())
+.then (data => {
+  const arr = data.message
+  arr.forEach(url => {
+    let img = document.createElement('IMG')
+    img.src = url
+    let images = document.getElementById('dog-image-container')
+    images.appendChild(img)
+  
+    })
+  })
+}
+function dogs() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+  .then(resp => resp.json())
+  .then (data => breeders(data))
+}
+  function breeders(data) {
+      for(let p in data.message) {
+      let dogId = document.getElementById('dog-breeds') 
+      let li = document.createElement('li')
+      li.innerText = p
+      dogId.appendChild(li)
+   }}
+       
+// when user clicks on any <li> the font color will change
+
+const l = document.getElementById('dog-breeds')
+const li = document. querySelectorAll('li')
+l.addEventListener('click', () => {
+ 
+ l.style.color = 'yellow'
+})
+
+
+})
