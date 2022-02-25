293b5363ff0eca7339c07b022ac1fb7f0babc346
Tanner <troxs690@gmail.com>
2022-02-25 16:50:44 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
    images () 
     dogs () 
     
+let dropdown = document.getElementById('breed-dropdown')
+let doggo = document.getElementById('dog-breeds')
+dropdown.addEventListener('change', (e) => {
+  matchingBreeds(e.target.value)
 })
+ function matchingBreeds(letter) {
+   console.log(dogArr)
+
+console.log("This is my", letter)
+// filter over dogArr, return all the elements that startWith() the specific letter
+
+
+
+ }
+let dogArr = []
 
   function images ()  {
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
@@ -27,26 +41,26 @@ function dogs() {
   .then (data => breeders(data))
 }
   function breeders(data) {
+    
+    dogArr = Object.keys(data.message)
+  
       for(let p in data.message) {
       let dogId = document.getElementById('dog-breeds') 
       let li = document.createElement('li')
       li.innerText = p
       dogId.appendChild(li)
+      li.style.cursor = 'pointer'
+      li.addEventListener('click', color)
+     
    }}
-       
-// when user clicks on any <li> the font color will change
-
-  let lol = document.querySelector('li')
-  lol.addEventListener('click', (e) => {
-    console.log(e.target)
+       function color(event) {
+event.target.style.color = 'Blue'
+  }
 
 
-
-  })
-      
-
-    
+  
 
 
 
+})
 
